631c1fd78b464ec613c25ed852e86a1fe4fb98ad
Leandro Luis <leandro.luis@uptc.edu.co>
2025-02-14 08:17:16 -0500

    feat: Add sum operation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese otro numero");
+        int b = scanner.nextInt();
+        System.out.println("La suma de los numeros es: " + sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        return a+b;
     }
 }
