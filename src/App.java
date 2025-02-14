ca1afa45215b121b6adfc7984abe7d03f51b5277
Leandro Luis <leandro.luis@uptc.edu.co>
2025-02-14 08:20:50 -0500

    feat: Add subtract operation

--- src/App.java
+++ src/App.java
@@ -11,6 +11,10 @@ public class App {
     }
 
     public static int sum(int a, int b) {
-        return a+b;
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
