0ad5d183e5e8b83ee393fd9103630fa31536ae8e
Leandro Luis <leandro.luis@uptc.edu.co>
2025-02-18 10:58:43 -0500

    borrar

--- src/App.java
+++ src/App.java
@@ -1,36 +1,5 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un numero");
-        int a = scanner.nextInt();
-        System.out.println("Ingrese otro numero");
-        int b = scanner.nextInt();
-        System.out.println("La suma de los numeros es: " + sum(a, b));
-        System.out.println("La resta de los numeros es: " + subtract(a, b));
-        System.out.println("El producto de los numeros es: " + multiplication(a, b));
-        if (b == 0) {
-            System.out.println("Eror: no es posible dividir por 0");
-        } else {
-            System.out.println("la division de los numeros es: " + division(a, b));
-        }
-    }
-
-    public static int sum(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public static int multiplication(int a, int b) {
-        return a * b;
-    }
-
-    public static double division(int a, int b) {
-        return a * b;
-    }
 
 }
