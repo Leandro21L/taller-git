b4b724356e6a16a6cb359cd5ba6bbfbcfd28615f
ROZ19K99 <mariafermedinamojica@gmail.com>
2025-02-18 11:12:23 -0500

    Agregar clase Suma para la calculadora

---
+++ src/suma.java
@@ -0,0 +1,13 @@
+public class suma {
+   
+     // Método para sumar dos números
+     public int sumar(int a, int b) {
+        return a + b;
+     }
+     public static void main(String[] args) {
+        suma Suma = new suma();
+        int resultado = Suma.sumar(5, 3);
+         System.out.println("El resultado de la suma es: " + resultado);
+        }
+    
+}
