6e02ac14d0e55b75a9b0d8e2cd845b03a4790230
ROZ19K99 <mariafermedinamojica@gmail.com>
2025-02-18 11:19:57 -0500

    Quitar impresion en suma

--- src/suma.java
+++ src/suma.java
@@ -4,10 +4,5 @@ public class suma {
      public int sumar(int a, int b) {
         return a + b;
      }
-     public static void main(String[] args) {
-        suma Suma = new suma();
-        int resultado = Suma.sumar(5, 3);
-         System.out.println("El resultado de la suma es: " + resultado);
-        }
     
 }
